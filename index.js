4370f0822ed260a859d006f6aa991ebfe403cea0
Pablo Fernández <93275138+Pablo-Programacion@users.noreply.github.com>
2022-07-10 16:25:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/* Borrar solamente uno */
+function borrar(text) {
+    let cadena = text;
+    let cadenaCorregida = cadena.substring(0, cadena.length - 1);
+    return cadenaCorregida
+}
+/* Hacer la raiz cuadrada */
+function raizCuadrada(text) {
+    return Math.sqrt(text)
+}
